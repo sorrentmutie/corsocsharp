fe7315ae66322f2bace42e7303688d946649a339
salvatore <salvatore.sorrentino@live.com>
2023-03-15 17:37:10 +0100

    Eventi

---
+++ BankAccount.cs
@@ -0,0 +1,81 @@
+﻿namespace DemoCorsoCSharp;
+
+public interface IAccountValidator
+{
+    void Validate(decimal amount);
+    event EventHandler<AccountArgs>? Validated;
+}
+
+public class SimpleValidator : IAccountValidator
+{
+    private  decimal top;
+
+    public SimpleValidator(decimal top)
+    {
+        this.top = top;
+    }
+
+    public event EventHandler<AccountArgs>? Validated;
+
+    public void Validate(decimal amount)
+    {
+        if(amount > top)
+        {
+            Validated?.Invoke(this, new AccountArgs { Message = "Messaggio 1" });
+        } else
+        {
+            Validated?.Invoke(this, new AccountArgs { Message = "Messaggio 2" });
+        }
+    }
+}
+
+
+public class AccountArgs: EventArgs
+{
+    public string? Message { get; set; }
+}
+
+
+
+
+public class BankAccount
+{
+    private IAccountValidator validator;
+
+
+    public BankAccount()
+    {
+        validator = new SimpleValidator(100000);
+        validator.Validated +=
+            (s, e) => _withdrawn?.Invoke(s, e);
+    }
+
+    public decimal Balance { get; private set; }
+    //public delegate void AccountHandler(object sender, AccountArgs args);
+    //public event AccountHandler? WithDrawn;
+
+    private EventHandler<AccountArgs>? _withdrawn;
+
+    public event EventHandler<AccountArgs>? WithDrawn
+    {
+        add { _withdrawn += value; }
+        remove { _withdrawn -= value; }
+    }
+
+
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+        validator.Validate(Balance);
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        Balance -= amount;
+        //if(Balance < 0)
+        //{
+        //    _withdrawn?.Invoke(this, new AccountArgs { Message = "ahia!"});
+        //}
+        validator.Validate(Balance);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -64,41 +64,41 @@
 ////}
 
 
-using DemoCorsoCSharp;
+//using DemoCorsoCSharp;
 
-//var lista = new List<int>();
-//for (int i = 0; i < 1000; i++)
+////var lista = new List<int>();
+////for (int i = 0; i < 1000; i++)
+////{
+////    lista.Add(i);
+////}
+
+////var somma = lista.SommaElementiPari();
+////var sommaSpeciale = lista.SommaElementiPariConParametro(12);
+
+
+//var listaPersone = new List<Person>();
+
+
+
+
+//var x = listaPersone.DoSomething(s => s.ToLower(), s => s.ToUpper(), "A");
+
+
+//var skip = 10;
+//var take = 5;
+
+//var persone = listaPersone
+//    .Where(p  => { return p.Name != null ? p.Name.ToUpper().StartsWith("S") : false; })
+//    .OrderBy(p => p.Name)
+//    .Skip(skip)
+//    .Take(take)
+//    .Select(p => new PersonDTO
 //{
-//    lista.Add(i);
-//}
+// Id = p.Id, FullName = $"{p.Name} {p.Surname}",
+//  FullAddress = $"{p.Address.StreetName} {p.Address.PostalCode}"
+//});
 
-//var somma = lista.SommaElementiPari();
-//var sommaSpeciale = lista.SommaElementiPariConParametro(12);
-
-
-var listaPersone = new List<Person>();
-
-
-
-
-var x = listaPersone.DoSomething(s => s.ToLower(), s => s.ToUpper(), "A");
-
-
-var skip = 10;
-var take = 5;
-
-var persone = listaPersone
-    .Where(p  => { return p.Name != null ? p.Name.ToUpper().StartsWith("S") : false; })
-    .OrderBy(p => p.Name)
-    .Skip(skip)
-    .Take(take)
-    .Select(p => new PersonDTO
-{
- Id = p.Id, FullName = $"{p.Name} {p.Surname}",
-  FullAddress = $"{p.Address.StreetName} {p.Address.PostalCode}"
-});
-
-persone.ToList().ForEach(Console.WriteLine);
+//persone.ToList().ForEach(Console.WriteLine);
 
 
 
@@ -119,8 +119,15 @@ persone.ToList().ForEach(Console.WriteLine);
 //delegate int D1(double a, string b);
 //// Func<double, string, int>,
 
+using DemoCorsoCSharp;
 
+var account = new BankAccount();
+account.WithDrawn += (s, e) => Console.WriteLine($"Messaggio: {e.Message}");
 
+account.Deposit(500);
+account.Withdraw(1000);
+
+Console.WriteLine($"Balance: {account.Balance}");
 
 
 
