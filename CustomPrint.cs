f56ce921737499e2ef19ae3fa98f5eb1de7f28c4
salvatore <salvatore.sorrentino@live.com>
2023-03-16 08:37:45 +0100

    Aggounta classe CustomPrint.cs per prima demo

---
+++ CustomPrint.cs
@@ -0,0 +1,34 @@
+﻿namespace MiaLibreria;
+
+public class CustomPrint
+{
+    private readonly string name = string.Empty;
+
+    public void Print() 
+    {
+        Console.WriteLine("Printing from Print()");
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public void PrintName()
+    {
+        Console.WriteLine($"Name set as {name}");
+    }
+
+    public void Print(string name )
+    {
+        Console.WriteLine($"Name passed: {name}");
+    }
+
+    private void PrintPrivate()
+    {
+        Console.WriteLine($"Printing from Private");
+    }
+
+    public string Name => name;
+    public static string StaticName => "Static Property Name";
+}
