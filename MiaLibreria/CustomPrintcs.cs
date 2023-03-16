8fd60d78998005a9c51e0080488e1714147e1721
salvatore <salvatore.sorrentino@live.com>
2023-03-16 08:29:02 +0100

    lambda

---
+++ MiaLibreria/CustomPrintcs.cs
@@ -0,0 +1,36 @@
+﻿namespace MiaLibreria;
+
+public class CustomPrints
+{
+    private readonly string name = string.Empty;
+
+    public void Print()
+    {
+        Console.WriteLine("Stampa da Print()");
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public void PrintName()
+    {
+        Console.WriteLine($"Nome impostato a {name}");
+    }
+
+    public void Print( string name )
+    {
+        Console.WriteLine($"Stampastringa passata {name}");
+    }
+
+    private void PrivatePrint()
+    {
+        Console.WriteLine("Stampa da metodo privato");
+    }
+
+    public string Name => name;
+    public static string StaticName => "Static Property";
+
+
+}
